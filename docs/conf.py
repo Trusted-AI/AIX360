cf03dda999eaa6f5d0de6778e8937addf1b7a9fb
vijay-arya <vijay.arya@in.ibm.com>
2019-08-04 18:49:49 +0530

    doc update

--- docs/conf.py
+++ docs/conf.py
@@ -10,9 +10,9 @@
 # add these directories to sys.path here. If the directory is relative to the
 # documentation root, use os.path.abspath to make it absolute, like shown here.
 #
-# import os
-# import sys
-# sys.path.insert(0, os.path.abspath('..'))
+import os
+import sys
+sys.path.insert(0, os.path.abspath('..'))
 import aix360
 
 
@@ -23,7 +23,7 @@ copyright = '2019, IBM Corporation'
 author = 'IBM Research'
 
 # The full version, including alpha/beta/rc tags
-release = aix360.__version__
+release = '0.1' #aix360.__version__
 
 
 # -- General configuration ---------------------------------------------------
