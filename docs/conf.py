a790bdae56101276a11440b302d74517be903b11
Vijay Arya <35213254+vijay-arya@users.noreply.github.com>
2019-08-04 18:44:59 +0530

    Update conf.py

--- docs/conf.py
+++ docs/conf.py
@@ -23,7 +23,7 @@ copyright = '2019, IBM Corporation'
 author = 'IBM Research'
 
 # The full version, including alpha/beta/rc tags
-release = aix360.__version__
+release = '0.1' #aix360.__version__
 
 
 # -- General configuration ---------------------------------------------------
