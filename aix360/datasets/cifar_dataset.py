67c04e9c23f77a19446fbed8e20c5f02733569fa
Moninder Singh <39064734+monindersingh@users.noreply.github.com>
2019-08-07 17:31:43 -0400

    CIFAR dataset change to automatically download data
    CIFAR dataset change to automatically download data and create processed data files

--- aix360/datasets/cifar_dataset.py
+++ aix360/datasets/cifar_dataset.py
@@ -1,7 +1,11 @@
 import numpy as np
 import json
 import sys,os
-
+import urllib.request
+import tarfile
+import pickle as cp
+from sklearn.preprocessing import OneHotEncoder
+import shutil
 
 class CIFARDataset():
     """
@@ -20,7 +24,99 @@ class CIFARDataset():
         if not self._dirpath:
             self._dirpath = os.path.join(os.path.dirname(os.path.abspath(__file__)),
                 '..', 'data','cifar_data')
+        self._download_data()
 
+    def _download_data(self):
+        name = 'cifar-10-python.tar.gz'
+        json_file_name = 'cifar-10-train1-image.json'
+        full_name = os.path.join(self._dirpath, name)
+        if not os.path.exists(os.path.join(self._dirpath, json_file_name)):
+            if not os.path.exists(full_name):
+                print("retrieving file", name)
+                urllib.request.urlretrieve('https://www.cs.toronto.edu/~kriz/' + name, full_name)
+                print("retrieved")
+            #now extract the files
+            print("extracting files")
+            tar = tarfile.open(full_name, "r:gz")
+            tar.extractall(self._dirpath)
+            tar.close()        
+            print("extracted files")
+            
+            self._process_data()
+
+            #now cleanup
+            if os.path.exists(full_name):
+                os.remove(full_name)
+            
+    def _process_data(self):
+        image_size=32
+        num_classes=10
+        per_file_size=10000
+        
+        datafile_path = os.path.join(self._dirpath, 'cifar-10-batches-py')
+        for i in range(5):
+            with open(os.path.join(datafile_path,'data_batch_'+str(i+1)), 'rb') as fileobj:
+                dictionary = cp.load(fileobj, encoding='bytes')
+                dum=dictionary[b'data'].reshape((per_file_size,3,32,32))
+                dum_1=np.transpose(dum,(0,2,3,1)).astype('uint8')
+                lab=np.asarray(dictionary[b'labels']).reshape((per_file_size,1))
+                if i==0:
+                    x_train=dum_1
+                    y_train=lab     
+                else:
+                    x_train=np.concatenate((x_train,dum_1),0)
+                    y_train=np.concatenate((y_train,lab),0)
+        
+        y_train=OneHotEncoder(sparse=False).fit_transform(y_train).astype('uint8')    
+                
+        assert x_train.shape==(5*per_file_size,image_size,image_size,3)
+        assert y_train.shape==(5*per_file_size,num_classes)
+        
+        x_train.astype(float)/255
+        
+        x_train_1=x_train[0:30000,:,:,:]
+        x_train_2=x_train[30000:,:,:,:]
+        y_train_1=y_train[0:30000,:]
+        y_train_2=y_train[30000:,:]
+        
+        
+        with open(os.path.join(datafile_path,'test_batch'), 'rb') as fileobj:
+            dictionary = cp.load(fileobj, encoding='bytes')
+            x_test=dictionary[b'data'].reshape((per_file_size,3,32,32))
+            x_test=np.transpose(x_test,[0,2,3,1]).astype('uint8')
+            y_test=np.asarray(dictionary[b'labels']).reshape((per_file_size,1))
+            y_test=OneHotEncoder(sparse=False).fit_transform(y_test).astype('uint8')
+        
+        with open(os.path.join(self._dirpath,'cifar-10-train1-image.json'),'w') as outfile:
+            json.dump(x_train_1.tolist(),outfile)
+        outfile.close()
+        
+        with open(os.path.join(self._dirpath,'./cifar-10-train2-image.json'),'w') as outfile:
+            json.dump(x_train_2.tolist(),outfile)
+        outfile.close()
+        
+        with open(os.path.join(self._dirpath,'./cifar-10-test-image.json'),'w') as outfile:
+            json.dump(x_test.tolist(),outfile)
+        outfile.close()
+        
+        with open(os.path.join(self._dirpath,'./cifar-10-train1-label.json'),'w') as outfile:
+            json.dump(y_train_1.tolist(),outfile)
+        outfile.close()
+        
+        with open(os.path.join(self._dirpath,'./cifar-10-train2-label.json'),'w') as outfile:
+            json.dump(y_train_2.tolist(),outfile)
+        outfile.close()
+        
+        with open(os.path.join(self._dirpath,'./cifar-10-test-label.json'),'w') as outfile:
+            json.dump(y_test.tolist(),outfile)
+        outfile.close()
+        #cleanup
+        if os.path.exists(datafile_path):
+            shutil.rmtree(datafile_path)
+        
+
+        
+                        
     def load_file(self, filename):
         try:
             with open(os.path.join(self._dirpath, filename)) as file:
