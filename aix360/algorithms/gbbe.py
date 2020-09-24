fd1dad3dea60b21b2109f3134d9d5c8b82cd4695
Vijay Arya <35213254+vijay-arya@users.noreply.github.com>
2020-09-24 18:49:08 +0530

    Create gbbe.py

---
+++ aix360/algorithms/gbbe.py
@@ -0,0 +1,47 @@
+import abc
+import sys
+
+# Ensure compatibility with Python 2/3
+if sys.version_info >= (3, 4):
+    ABC = abc.ABC
+else:
+    ABC = abc.ABCMeta(str('ABC'), (), {})
+
+
+class GlobalBBExplainer(ABC):
+    """
+    GlobalBBExplainer is the base class for Global black-box explainers (GBBE).
+    Given a source black box model, such explainers generally train a surrogate 
+    model that is explainable.
+
+    References:
+        .. [#1] Model Agnostic Multilevel Explanations,
+        Karthikeyan Natesan Ramamurthy, Bhanukiran Vinzamuri, Yunfeng Zhang, Amit Dhurandhar.
+        https://arxiv.org/abs/2003.06005
+    """
+
+    def __init__(self, *argv, **kwargs):
+        """
+        Initialize a GlobalBBExplainer object.
+        """
+
+    @abc.abstractmethod
+    def set_params(self, *argv, **kwargs):
+        """
+        Set parameters for the explainer.
+        """
+        raise NotImplementedError
+
+    @abc.abstractmethod
+    def explain(self, *argv, **kwargs):
+        """
+        Explain model.
+        """
+        raise NotImplementedError
+
+    @abc.abstractmethod
+    def fit(self, *argv, **kwargs):
+        """
+        Train a surrogate model.
+        """
+        raise NotImplementedError
