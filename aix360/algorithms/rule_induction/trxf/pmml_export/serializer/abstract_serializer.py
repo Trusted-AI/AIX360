d5d9dc7586689243fb6f312b930dceb7659ca478
ke li <Ke.Li1@ibm.com>
2022-10-21 09:30:44 +0200

    Add aix360.algorithms.rule_induction.trxf.pmml_export.serializer.abstract_serializer.AbstractSerializer.serialize_scorecard
    Signed-off-by: Ke LI <ke.li1@ibm.com>

--- aix360/algorithms/rule_induction/trxf/pmml_export/serializer/abstract_serializer.py
+++ aix360/algorithms/rule_induction/trxf/pmml_export/serializer/abstract_serializer.py
@@ -8,3 +8,7 @@ class AbstractSerializer(abc.ABC):
     @abc.abstractmethod
     def serialize(self, simple_pmml_ruleset_model: models.SimplePMMLRuleSetModel) -> str:
         raise NotImplementedError
+
+    @abc.abstractmethod
+    def serialize_scorecard(self, simple_pmml_ruleset_model: models.Scorecard) -> str:
+        raise NotImplementedError
