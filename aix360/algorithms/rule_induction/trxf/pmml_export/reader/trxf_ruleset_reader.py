a409d767b8397a0da6738022a44d77388b48dfc9
Yusik Kim <kmyusk@gmail.com>
2022-11-02 19:42:13 +0100

    Implemented trxf scorecard reader for PMML export
    Signed-off-by: Yusik Kim <kmyusk@gmail.com>

--- aix360/algorithms/rule_induction/trxf/pmml_export/reader/trxf_ruleset_reader.py
+++ aix360/algorithms/rule_induction/trxf/pmml_export/reader/trxf_ruleset_reader.py
@@ -1,19 +1,13 @@
-from typing import Dict
-
-import numpy as np
 import pandas as pd
 
 from aix360.algorithms.rule_induction.trxf.classifier import ruleset_classifier
 from aix360.algorithms.rule_induction.trxf.classifier.ruleset_classifier import RuleSetClassifier
-from aix360.algorithms.rule_induction.trxf.core import Conjunction, Relation
 from aix360.algorithms.rule_induction.trxf.pmml_export import models
-from aix360.algorithms.rule_induction.trxf.pmml_export.models.data_dictionary import Value
 from aix360.algorithms.rule_induction.trxf.pmml_export.reader import AbstractReader
-from aix360.algorithms.rule_induction.trxf.pmml_export.models import SimplePredicate, Operator, CompoundPredicate, \
-    BooleanOperator
+from aix360.algorithms.rule_induction.trxf.pmml_export.utilities import extract_data_dictionary, trxf_to_pmml_predicate
 
 
-class TrxfReader(AbstractReader):
+class TrxfRuleSetReader(AbstractReader):
     def __init__(self, data_dictionary=None):
         self._data_dictionary = data_dictionary
 
@@ -48,35 +42,13 @@ class TrxfReader(AbstractReader):
         @param X: Input dataframe
         @param values: A dict mapping column name to a list of possible categorical values. It will be inferred from X if not provided.
         """
-        dtypes = X.dtypes
-        data_fields = []
-        for index, value in dtypes.items():
-            vals = None
-            if np.issubdtype(value, np.integer):
-                data_type = models.DataType.integer
-                op_type = models.OpType.ordinal
-            elif np.issubdtype(value, np.double):
-                data_type = models.DataType.double
-                op_type = models.OpType.continuous
-            elif np.issubdtype(value, np.floating):
-                data_type = models.DataType.float
-                op_type = models.OpType.continuous
-            elif np.issubdtype(value, np.bool_):
-                data_type = models.DataType.boolean
-                op_type = models.OpType.categorical
-            else:
-                data_type = models.DataType.string
-                op_type = models.OpType.categorical
-                vals = values[index] if values is not None and index in values else list(X[index].unique())
-            wrapped_vals = list(map(lambda v: Value(v), vals)) if vals is not None else vals
-            data_fields.append(models.DataField(name=str(index), optype=op_type, dataType=data_type, values=wrapped_vals))
-        self._data_dictionary = models.DataDictionary(data_fields)
+        self._data_dictionary = extract_data_dictionary(X)
 
 
 def _convert_to_simple_rules(trxf_rules):
     simple_rules = []
     for rule in trxf_rules:
-        predicate = _convert_to_pmml_predicate(rule.conjunction)
+        predicate = trxf_to_pmml_predicate(rule.conjunction)
         confidence = rule.confidence if rule.confidence is not None else models.DEFAULT_CONFIDENCE
         weight = rule.weight if rule.weight is not None else models.DEFAULT_WEIGHT
         simple_rule = models.SimpleRule(predicate=predicate, score=str(rule.label), id=str(rule.conjunction),
@@ -84,22 +56,6 @@ def _convert_to_simple_rules(trxf_rules):
                                         nbCorrect=rule.nb_correct, confidence=confidence, weight=weight)
         simple_rules.append(simple_rule)
     return simple_rules
-
-
-def _convert_to_pmml_predicate(trxf_conjunction: Conjunction):
-    trxf_to_pmml_op = {
-        Relation.EQ: Operator.equal,
-        Relation.NEQ: Operator.notEqual,
-        Relation.LT: Operator.lessThan,
-        Relation.LE: Operator.lessOrEqual,
-        Relation.GT: Operator.greaterThan,
-        Relation.GE: Operator.greaterOrEqual
-    }
-    simple_predicates = [SimplePredicate(operator=trxf_to_pmml_op[trxf_predicate.relation],
-                                         value=str(trxf_predicate.value),
-                                         field=str(trxf_predicate.feature.variable_names[0]))
-                         for trxf_predicate in trxf_conjunction.predicates]
-    return CompoundPredicate(simplePredicates=simple_predicates, booleanOperator=BooleanOperator.and_)
 
 
 def _extract_mining_schema(trxf_rules):
--- tests/rule_induction/trxf/pmml_export/test_trxf_reader.py
+++ tests/rule_induction/trxf/pmml_export/test_trxf_reader.py
@@ -1,16 +1,18 @@
 from unittest import TestCase
 
 import aix360.algorithms.rule_induction.trxf.classifier.ruleset_classifier as classifier
+import aix360.algorithms.rule_induction.trxf.scorecard as sc
 from aix360.algorithms.rule_induction.trxf.classifier.ruleset_classifier import RuleSetClassifier
 from aix360.algorithms.rule_induction.trxf.pmml_export.models import RuleSetModel, SimplePMMLRuleSetModel
-from aix360.algorithms.rule_induction.trxf.pmml_export.reader.trxf_reader import TrxfReader
+from aix360.algorithms.rule_induction.trxf.pmml_export.reader.trxf_ruleset_reader import TrxfRuleSetReader
+from aix360.algorithms.rule_induction.trxf.pmml_export.reader.trxf_scorecard_reader import TrxfScorecardReader
 from tests.rule_induction.trxf.utilities import create_test_ruleset, DATA_DICTIONARY, TEST_MINING_SCHEMA, \
-    TEST_PMML_RULESET, DATA_FRAME
+    TEST_PMML_RULESET, DATA_FRAME, create_test_scorecard, PARTITIONS, DATA_DICTIONARY_SC
 
 
 class TestTrxfReader(TestCase):
-    def test_read(self):
-        reader = TrxfReader(DATA_DICTIONARY)
+    def test_read_ruleset(self):
+        reader = TrxfRuleSetReader(DATA_DICTIONARY)
         test_ruleset = create_test_ruleset()
         test_classifier = RuleSetClassifier([test_ruleset], classifier.RuleSelectionMethod.FIRST_HIT, default_label=0)
         ruleset_model = RuleSetModel(miningSchema=TEST_MINING_SCHEMA, ruleSet=TEST_PMML_RULESET)
@@ -18,6 +20,13 @@ class TestTrxfReader(TestCase):
         self.assertEqual(reader.read(test_classifier), expected)
 
     def test_load_data_dictionary(self):
-        reader = TrxfReader()
+        reader = TrxfRuleSetReader()
         reader.load_data_dictionary(DATA_FRAME)
         self.assertEqual(reader.data_dictionary, DATA_DICTIONARY)
+
+    def test_read_scorecard(self):
+        expected = create_test_scorecard()
+        scorecard = sc.Scorecard(PARTITIONS, bias=100)
+        reader = TrxfScorecardReader(DATA_DICTIONARY_SC)
+        actual = reader.read(scorecard)
+        self.assertEqual(expected, actual)
