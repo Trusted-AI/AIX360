e07212515fb1631b8e0987bffd4bfc9caef0e4c5
Swagatam Haldar <swagatam.haldar9@gmail.com>
2024-06-06 19:40:20 +0200

    ecertify files

---
+++ tests/ecertify/test_ecertify.py
@@ -0,0 +1,18 @@
+import unittest
+import numpy as np
+
+from aix360.algorithms.ecertify.ExpCertifyBB import Certify
+
+class TestCertifyExplainer(unittest.TestCase):
+
+    def test_for_low_dim_toy_problem(self):
+        # test all strategies for dimensions 1, 2 and 5?
+        pass
+
+    def test_for_fico(self):
+        # just run
+        pass
+
+
+if __name__ == '__main__':
+    unittest.main()
