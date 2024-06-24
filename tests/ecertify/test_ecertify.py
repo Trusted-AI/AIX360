27b8d69996042a5872bd771a07a741a550f3c82d
Swagatam Haldar <swagatam.haldar9@gmail.com>
2024-06-24 08:39:24 +0200

    added utils for lime/shap

--- tests/ecertify/test_ecertify.py
+++ tests/ecertify/test_ecertify.py
@@ -5,8 +5,21 @@ from aix360.algorithms.ecertify.ExpCertifyBB import Certify
 
 class TestCertifyExplainer(unittest.TestCase):
 
+    def test_all_imports(self):
+        from aix360.algorithms.ecertify.ExpCertifyBB import Certify, Ecertify
+        from aix360.algorithms.ecertify.utils import compute_lime_explainer, compute_shap_explainer
+
     def test_for_low_dim_toy_problem(self):
-        # test all strategies for dimensions 1, 2 and 5?
+        #Calling Function
+        d = 2 #input dimensionality
+        x = np.array([0.0]*d) #input
+        theta = 0.75 #fidelity threshold
+        Z = 10 #number of hypercubes to certify
+        Q = 1000 #query budget for each hypercube
+        eps = 0.1/d #min gap between lb and ub
+        numruns = 10
+
+        
         pass
 
     def test_for_fico(self):
